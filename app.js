c1bb18813f0d26ddf5c4a34f966d3d0f69742208
Lucy Donnelly <lucy.donnelly@autotrader.co.uk>
2021-06-28 21:51:04 +0100

    add  square with colour change

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
-const button = document.getElementById("button")
-const emptySpanForColour = document.getElementById("container__content--span")
-
+const button = document.getElementById("button");
+const emptySpanForColour = document.getElementById("container__content--span");
+const squareOfColour = document.getElementById("squareOfColour")
+const colours = ['green', 'blue', 'yellow', 'orange', 'purple']
 
 
 
 //functions
 
 const changeBackgroundColour = (event) => {
-    let currentColor = "green";
+    let currentColor = colours[Math.floor(Math.random() * 5)]
+
+    let colorWithCapitalFirstLetter = (currentColor.slice(0, 1)).toUpperCase() + currentColor.slice(1)
+
     document.body.style.background = currentColor;
-    emptySpanForColour.innerHTML = currentColor
+    emptySpanForColour.innerHTML = colorWithCapitalFirstLetter; 
      
 }
 
+const changeSquareColour = () => {
+    let currentColor = colours[Math.floor(Math.random() * 5)]
+    squareOfColour.style.backgroundColor = currentColor;
+
+}
 
 //event listener 
 
 button.addEventListener("click", changeBackgroundColour)
+button.addEventListener("click", changeSquareColour); 
